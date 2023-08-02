f0f0abab1e26b81d3d8dd2312afbdd62919ee6dd
Mangesh29 <bhosalemangesh260@gmail.com>
2023-08-02 07:36:17 +0530

    method addtion

--- flaskbasic.py
+++ flaskbasic.py
@@ -9,6 +9,31 @@ app = Flask(__name__)
 def home_page():
     return render_template("index.html")
 
+@app.route('/math',methods=['POST'])
+def math_ops():
+    if(request.method == 'POST'):
+        ops = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        if(ops == 'add'):
+            r = num1+ num2
+            addresult= "sum  of "+ str(num1) + str(num2) +"is"+str(r)
+
+        if(ops == 'subtract'):
+            r = num1- num2
+            addresult= "subtract is "+ str(num1) + str(num2) +"is"+str(r)
+
+        if(ops == 'multiply'):
+            r = num1* num2
+            addresult= "multiply  of "+ str(num1) + str(num2) +"is"+str(r)
+
+        if(ops == 'divide'):
+            r = num1/ num2
+            addresult= "divide by "+ str(num1) + str(num2) +"is"+str(r)
+                
+        return render_template('result.html',result=addresult)   
+
+
 # @app.route("/")
 # def hello_world():
 #     return "<h1 hello, world </h1>"
