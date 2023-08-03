c7d39767f1653002cc7e0b1753be12e8e447948d
Mangesh29 <bhosalemangesh260@gmail.com>
2023-08-03 18:55:22 +0530

    method addtion

--- flaskbasic.py
+++ flaskbasic.py
@@ -17,7 +17,7 @@ def math_ops():
         num2 = int(request.form['num2'])
         if(ops == 'add'):
             r = num1+ num2
-            addresult= "sum  of "+ str(num1) + str(num2) +"is"+str(r)
+            addresult= "sum  of "+ str(num1) + str(num2) +"    is"+str(r)
 
         elif(ops == 'subtract'):
             r = abs(num1- num2)
@@ -32,6 +32,35 @@ def math_ops():
             addresult= "divide by "+ str(num1) + str(num2) +"is"+str(r)
                 
         return render_template('result.html',result=addresult)   
+    
+
+@app.route('/mathwithpostman',methods=['POST'])
+def calaulationpostman():
+    if(request.method == 'POST'):
+        ops = request.json['operation']
+        num1 = int(request.json['num1'])
+        num2 = int(request.json['num2'])
+    if(request.method == 'GET'):
+        ops = request.arg.get('operation')
+        num1 = int(request.arg.get('num1'))
+        num2 = int(request.arg.get('num2'))
+        if(ops == 'add'):
+            r = num1+ num2
+            addresult= "sum  of "+ str(num1) + str(num2) +"is"+str(r)
+
+        elif(ops == 'subtract'):
+            r = abs(num1- num2)
+            addresult= "subtract of "+ str(num1) + str(num2) +"is"+str(r)
+
+        elif(ops == 'multiply'):
+            r = num1* num2
+            addresult= "multiply  of "+ str(num1) + str(num2) +"is"+str(r)
+
+        elif(ops == 'divide'):
+            r = num1/ num2
+            addresult= "divide by "+ str(num1) + str(num2) +"is"+str(r)
+                
+        return  jsonify(addresult)  
 
 
 # @app.route("/")
@@ -43,9 +72,11 @@ def math_ops():
 #     data = request.args.get('x')
 #     return "this is data input from my url :{}".format(data)
 
-# @app.route("/login")
-# def test3():
-#     return "<h1>logeed in!</h1>"
+@app.route("/useParam")
+def useParam():
+   queryParam= request.arg.get('x')
+   return "Query parameter sent is :{}".formate(queryParam)
+
 
 if __name__ =="__main__":
     app.run(host="0.0.0.0")
